0c78af6b98bac14dd803beb52abc9fe9c65380db
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-07-01 13:17:48 +0200

    4

--- src/components/statistics/Statistics.styled.jsx
+++ src/components/statistics/Statistics.styled.jsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import styled from 'styled-components';
 import { getRandomHexColor } from 'until/getrandomcalor.js';
 
 export const Section = styled.section`
