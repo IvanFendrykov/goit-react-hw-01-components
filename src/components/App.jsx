a28e1a0b5b2377eeaf9e8e200319597c5f0b6b55
IvanFendrykov <118548628+IvanFendrykov@users.noreply.github.com>
2023-07-03 00:56:27 +0200

    10

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,13 @@
-import user from '../data/user.json';
+import user from './data/user.json';
 import { Profile } from './profile/Profile';
 
-import data from '../data/data.json';
+import data from './data/data.json';
 import { Statistics } from './statistics/Statistics';
 
-import friends from '../data/friends.json';
+import friends from './data/friends.json';
 import {FriendList} from './friendlist/FriendList';
 
-import transactions from '../data/transactions.json';
+import transactions from './data/transactions.json';
 import { TransactionHistory } from './transactionhistory/TransactionHistory';
 
 import {Box} from './Box.js'
